a22350196905a8e6813f03c64c2c2c9212c9d8b2
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2025-11-12 09:37:57 +0100

    Finished up the discover_devices_simple example

--- src/discover_devices_simple.c
+++ src/discover_devices_simple.c
@@ -19,10 +19,40 @@ int main()
     daqList* availableDevices = NULL;
     discoverDevices(availableDevices, instance);
 
-    // TODO: Add a way to display the found devices
+    daqIterator* iterator = NULL;
+    daqList_createStartIterator(availableDevices, &iterator);
+
+    // Go through all detected devices and print out their respective domain info
+    while (daqIterator_moveNext(iterator) == DAQ_SUCCESS)
+    {
+        daqDeviceInfo* currentDevInfo = NULL;
+        daqIterator_getCurrent(iterator, (daqBaseObject*)&currentDevInfo);
+        
+        daqString* name = NULL;
+        daqDeviceInfo_getName(currentDevInfo, &name);
+
+        daqString* manufacturer = NULL;
+        daqDeviceInfo_getManufacturer(currentDevInfo, &manufacturer);
+
+        daqConstCharPtr* name_constChar = NULL;
+        daqString_getCharPtr(name, &name_constChar);
+
+        daqConstCharPtr* manufacturer_constChar = NULL;
+        daqString_getCharPtr(manufacturer, &name_constChar);
+
+        printf("Name of the device: %s\n", name_constChar);
+        printf("Manufactorer of the device: %s\n", manufacturer_constChar);
+
+        daqReleaseRef(currentDevInfo);
+        daqReleaseRef(name);
+        daqReleaseRef(manufacturer);
+        daqReleaseRef(name_constChar);
+        daqReleaseRef(manufacturer_constChar);
+    }
 
     daqReleaseRef(instance);
     daqReleaseRef(availableDevices);
+    daqReleaseRef(iterator);
 
     return 0;
 }
