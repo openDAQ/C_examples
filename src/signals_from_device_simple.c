eb1049eb73a72ba7f43c21cc64eda36c11d1d6c0
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2025-11-12 16:16:35 +0100

    Cmake setup + signals_from_device_simple example improvements

--- src/signals_from_device_simple.c
+++ src/signals_from_device_simple.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 #include <connect_device_simple.c>
 
-void signalsFromDevices(daqDevice* device, daqSignal* signal)
+void signalsFromDevices(daqInstance* instance, daqDevice* device, daqSignal* signal)
 {
     // Connect to the device and connect the signal
-
+    daqList* signals = NULL;
+    daqFunctionBlock* channel = NULL;
+    daqList* channels = NULL;
+    daqDevice_getChannels(device, &channels, NULL);
+    
 }
 
 int main()
@@ -20,10 +24,22 @@ int main()
     connectDevice(availableDevices, instance, connectedDevice);
 
     daqSignal* signal = NULL;
-    signalsFromDevices(connectedDevice, signal);
+    signalsFromDevices(instance, connectedDevice, signal);
 
+    daqDataDescriptor* signalDescriptor = NULL;
+    daqSignal_getDescriptor(signal, &signalDescriptor);
 
+    daqString* signalName = NULL;
+    daqDataDescriptor_getName(signalDescriptor, &signalName);
 
+    daqConstCharPtr* signalName_constChar = NULL;
+    daqString_getCharPtr(signalName, &signalName_constChar);
+
+    printf("The name of the connected signal is: %s\r\n", signalName_constChar);
+
+    daqReleaseRef(signalDescriptor);
+    daqReleaseRef(signalName);
+    daqReleaseRef(signalName_constChar);
     daqReleaseRef(instance);
     daqReleaseRef(availableDevices);
     daqReleaseRef(connectedDevice);
