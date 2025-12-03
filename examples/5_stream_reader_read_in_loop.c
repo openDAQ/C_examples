60226d080b703a09599f64b24541f73cdc698d02
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2025-12-03 11:40:54 +0100

    First steps into 5th example

--- examples/5_stream_reader_read_in_loop.c
+++ examples/5_stream_reader_read_in_loop.c
@@ -1,9 +1,27 @@
 /*
- * 
+ * Example show how to create, connect and read data with an OpenDAQ streamReader 
  */
 #include <daq_utils.h>
 
+void connectToFirstAvailableSignal(daqSignal** signal, daqDevice* device)
+{
+    
+}
+
 int main(void)
 {
+    daqInstance* simulatorInstance = NULL;
+    setupSimulator(&simulatorInstance);
+    daqInstance* instance = NULL;
+    daqDevice* simulator = NULL;
+    addSimulator(&simulator, &instance);
+
+    daqSignal* connectedSignal = NULL;
+    connectToFirstAvailableSignal(&connectedSignal, simulator);
+    // Use the skeleton of the application_example.c, however use read instead of read_with_domain
+
+    daqReleaseRef(instance);
+    daqReleaseRef(simulator);
+    daqReleaseRef(simulatorInstance);
     return 0;
 }
