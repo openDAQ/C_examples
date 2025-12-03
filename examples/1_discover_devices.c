bb3e096f4e19525dfbe7f7c00eccd18bcddfed75
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2025-12-03 08:45:46 +0100

    Fixed todos

--- examples/1_discover_devices.c
+++ examples/1_discover_devices.c
@@ -1,30 +1,23 @@
+/*
+ * This example demonstrated how to create an openDAQ instance and discover all available devices for connection
+ */
+
 #include <daq_utils.h>
 
-// TODO: Move up
-// This example demonstrated how to create an openDAQ instance and discover all available devices for connection
 
 int main(void)
 {
     daqInstanceBuilder* builder = NULL;
     daqInstanceBuilder_createInstanceBuilder(&builder);
 
-    // The created Instance gets its settings from the builder that was used to create it
     daqInstance* instance = NULL;
     daqInstance_createInstanceFromBuilder(&instance, builder);
 
-    // Discovering devices:
-
-    // Calling the function that discovers all the available devices for connection
     daqList* availableDevices = NULL;
     daqDevice_getAvailableDevices((daqDevice*)instance, &availableDevices);
 
-    // Due to the nature of the C language we need to create an Iterator object when traversing the daqList object
     daqIterator* iterator = NULL;
     daqList_createStartIterator(availableDevices, &iterator);
- 
-    // TODO: A bit too many comments
-    // OpenDAQ iterators need to be "booted up" with a daqIterator_moveNext call as they do not 
-    // start pointing to the first element of the list in the beginning.
    
     // Go through all detected devices and print out their respective domain info
     while (daqIterator_moveNext(iterator) == DAQ_SUCCESS)
