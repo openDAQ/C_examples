5a6d6633dfe1f202bb1d4c33561bf802333c7ddb
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2025-12-04 09:07:07 +0100

    First proper iteration of 6th example

--- examples/6_read_with_formatted_timestamps.c
+++ examples/6_read_with_formatted_timestamps.c
@@ -12,21 +12,66 @@ int main(void)
     daqDevice* simulator = NULL;
     addSimulator(&simulator, &instance);
 
-    daqList* availableDevices;
-    daqDevice_getSignalsRecursive(simulator, &availableDevices, NULL);
+    daqList* availableSignals;
+    daqDevice_getSignalsRecursive(simulator, &availableSignals, NULL);
 
     daqSignal* signal = NULL;
-    daqList_getItemAt(availableDevices, 0, (daqBaseObject**)signal);
+    daqList_getItemAt(availableSignals, 1, (daqBaseObject**)&signal);
 
-    // Connect to the first signal,
-    // Read with domain,
-    // Display the readings with accompanying time
+    daqStreamReader* streamReader = NULL;
+    daqStreamReader_createStreamReader(&streamReader, signal, daqSampleTypeFloat64, daqSampleTypeInt64, daqReadModeRawValue, daqReadTimeoutTypeAny);
+
+    daqSignal* domainSignal = NULL;
+    daqSignal_getDomainSignal(signal, &domainSignal);
+
+    daqDataDescriptor* dataDescriptor = NULL;
+    daqSignal_getDescriptor(domainSignal, &dataDescriptor);
+
+    daqRatio* ratio = NULL;
+    daqDataDescriptor_getTickResolution(dataDescriptor, &ratio);
+
+    daqUnit* unit = NULL;
+    daqDataDescriptor_getUnit(dataDescriptor, &unit);
+
+    daqString* unitSymbol = NULL;
+    daqUnit_getSymbol(unit, &unitSymbol);
+
+    daqConstCharPtr unitSymbolCostChar = NULL;
+    daqString_getCharPtr(unitSymbol, &unitSymbolCostChar);
+
+    daqUInt domainSamples[100];
+    daqFloat samples[100];
+    const daqSizeT timeoutMs = 1000;
+
+    for (int i = 0; i< 40; i++)
+    {
+        daqSizeT count = 100;
+        daqStreamReader_readWithDomain(streamReader, samples, domainSamples, &count, timeoutMs, NULL);
+        if (count > 0)
+        {
+            daqInt resolutionNumerator = 0;
+            daqInt resolutionDenominator = 0;
+            daqRatio_getNumerator(ratio, &resolutionNumerator);
+            daqRatio_getDenominator(ratio, &resolutionDenominator);
+
+            daqFloat domainValue = (daqFloat)domainSamples[count - 1] * resolutionNumerator / resolutionDenominator;
+            printf("Value: %f, Domain: %f%s\n", samples[count - 1], domainValue, unitSymbolCostChar);
+        }
+    }
+
+    daqReleaseRef(unitSymbol);
+    daqReleaseRef(unit);
+    daqReleaseRef(ratio);
+    daqReleaseRef(dataDescriptor);
+    daqReleaseRef(domainSignal);
+    daqReleaseRef(streamReader);
 
     daqReleaseRef(signal);
-    daqReleaseRef(availableDevices);
+    daqReleaseRef(availableSignals);
     daqReleaseRef(simulator);
     daqReleaseRef(instance);
     daqReleaseRef(simulatorInstance);
+
     return 0;
 }
  
