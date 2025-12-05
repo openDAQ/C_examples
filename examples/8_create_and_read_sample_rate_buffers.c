b8b105439026ea64c75611cf8decca6909a70993
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2025-12-05 11:54:36 +0100

    Skeleton of the 8th example

--- examples/8_create_and_read_sample_rate_buffers.c
+++ examples/8_create_and_read_sample_rate_buffers.c
@@ -1,10 +1,29 @@
 /*
- *
+ * This example will demonstrate how to create and read into buffers
+ * that are created based on the calculated sample rate
  */
 #include <daq_utils.h>
 
 int main(void)
 {
-    
+    daqInstance* simulatorInstance = NULL;
+    setupSimulator(&simulatorInstance);
+
+    daqInstance* instance = NULL;
+    daqDevice* simulator = NULL;
+    addSimulator(&simulator, &instance);
+
+    // Calculate sample rate
+
+    // Create sample rate buffer
+
+    // Create reader
+
+    // Read into the created buffer
+
+    daqReleaseRef(instance);
+    daqReleaseRef(simulator);
+    daqReleaseRef(simulatorInstance);
+
     return 0;
 }
