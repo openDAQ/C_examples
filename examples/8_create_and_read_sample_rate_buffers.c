8a96ec0d384e2ec2a0509d9d27ae78ae1077dc73
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2025-12-18 13:39:27 +0100

    Addressed the code review
    Added an exit clause for gathering the signals

--- examples/8_create_and_read_sample_rate_buffers.c
+++ examples/8_create_and_read_sample_rate_buffers.c
@@ -15,7 +15,13 @@ int main(void)
 
     // Connect to the first available signal on the simulator
     daqList* availableSignals = NULL;
-    daqDevice_getSignalsRecursive(simulator, &availableSignals, NULL);
+    if (daqDevice_getSignalsRecursive(simulator, &availableSignals, NULL) != DAQ_SUCCESS)
+    {
+        daqReleaseRef(simulator);
+        daqReleaseRef(instance);
+        daqReleaseRef(simulatorInstance);
+        return 1;
+    }
 
     daqSignal* connectedSignal = NULL;
     daqList_getItemAt(availableSignals, 0, (daqBaseObject**)&connectedSignal);
