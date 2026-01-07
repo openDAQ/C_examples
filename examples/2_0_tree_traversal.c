1e67a957f62108a40bb4851d5b0f3037364b4273
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2026-01-07 11:21:02 +0100

    tree_traversal example update

--- examples/2_0_tree_traversal.c
+++ examples/2_0_tree_traversal.c
@@ -1,8 +1,6 @@
 /*
- * In this example we will demonstrate the structure of openDAQ tree structure 
- * by traversing across it. 
- *
- *
+ * In this example we will demonstrate the structure of openDAQ tree structure
+ * by traversing across it.
  */
 
 #include <daq_utils.h>
@@ -17,6 +15,40 @@ int main()
     addSimulator(&simulator, &instance);
 
     // Example code
+    daqList* listOfComponents = NULL;
+    daqList_createList(&listOfComponents);
+    // Go through all the folders that are available on the device
+    daqFolder* simulatorIOFolder = NULL;
+    daqDevice_getInputsOutputsFolder(simulator, &simulatorIOFolder);
+
+    daqList* simulatorIOItems = NULL;
+    daqSearchFilter* filterIOAny = NULL;
+    daqSearchFilter_createAnySearchFilter(&filterIOAny);
+    daqFolder_getItems(simulatorIOFolder, &simulatorIOItems, filterIOAny);
+
+    daqIterator* ioItemsIterator = NULL;
+    daqList_createStartIterator(simulatorIOItems, &ioItemsIterator);
+
+    while (daqIterator_moveNext(ioItemsIterator) == DAQ_SUCCESS)
+    {
+        daqDataDescriptor* ioItem = NULL;
+        daqIterator_getCurrent(ioItemsIterator, &ioItem);
+
+        daqDict* itemMetadata = NULL;
+        daqDataDescriptor_getMetadata(ioItem, &itemMetadata);
+
+
+    }
+
+    daqReleaseRef(ioItemsIterator);
+    daqReleaseRef(filterIOAny);
+    daqReleaseRef(simulatorIOItems);
+    daqReleaseRef(simulatorIOFolder);
+    // Search for all the signals
+
+    // Check for additional devices
+
+    // Repeat the procedure for all connected devices (recursively)
 
     daqReleaseRef(instance);
     daqReleaseRef(simulator);
