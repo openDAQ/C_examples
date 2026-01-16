72765daff02ff5d733fbf6bd224251944fd4a68f
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2026-01-16 08:49:34 +0100

    Updated example and removed dangling comments

--- examples/util_headers/daq_utils.h
+++ examples/util_headers/daq_utils.h
@@ -163,7 +163,7 @@ static inline daqErrCode setupSimulator(daqInstance** instance)
 
     daqInstanceBuilder_setGlobalLogLevel(instanceBuilder, daqLogLevelOff);
 
-    daqErrCode err = 0;//daqInstance_createInstanceFromBuilder(instance, instanceBuilder);
+    daqErrCode err = 0;
     err = daqInstanceBuilder_build(instanceBuilder, instance);
 
     if (err != 0)
