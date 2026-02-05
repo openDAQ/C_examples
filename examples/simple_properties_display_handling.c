5bd6255fb09e492a07a4b448f1331ee24f02be9e
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2026-02-05 11:03:05 +0100

    Added missing processing

--- examples/simple_properties_display_handling.c
+++ examples/simple_properties_display_handling.c
@@ -5,18 +5,24 @@
 #include <daq_c_conversions.h>
 
 void determinePropertyTypeAndPrint(daqProperty* property);
-void displayPropertyTypes(daqPropertyObject* propertyObject);
-void displayCommonMetadata(daqProperty* property);
+void printPropertyObject(daqPropertyObject* propertyObject);
+void printMetadata(daqProperty* property);
 void intProp(daqProperty* property);
 void floatProp(daqProperty* property);
 void stringProp(daqProperty* property);
 void boolProp(daqProperty* property);
 void sparseSelectionProp(daqProperty* property);
 void selectionProp(daqProperty*  property);
+void ratioProp(daqProperty* property);
+void functionProp(daqProperty* property);
+void objectProp(daqProperty* property);
+void prop(daqProperty* property);
+void printSelectedValue(daqBaseObject* container, daqCoreType itemType, daqInteger* keyValue);
+
 
 void determinePropertyTypeAndPrint(daqProperty* property)
 {
-    displayCommonMetadata(property);
+    printMetadata(property);
     daqCoreType propCoreType = daqCtUndefined;
     daqProperty_getValueType(property, &propCoreType);
     switch(propCoreType)
@@ -42,12 +48,33 @@ void determinePropertyTypeAndPrint(daqProperty* property)
             selectionProp(property);
         break;
     }
+    case daqCtFunc:
+    {
+        functionProp(property);
+        break;
+    }
+    case daqCtProc:
+    {
+        functionProp(property);
+        break;
+    }
+    case daqCtRatio:
+    {
+        ratioProp(property);
+        break;
+    }
+    case daqCtObject:
+    {
+        objectProp(property);
+        break;
+    }
     default:
+        prop(property);
         break;
     }
 }
 
-void displayPropertyTypes(daqPropertyObject* propertyObject)
+void printPropertyObject(daqPropertyObject* propertyObject)
 {
     daqList* properties = NULL;
     daqPropertyObject_getVisibleProperties(propertyObject, &properties);
@@ -63,13 +90,14 @@ void displayPropertyTypes(daqPropertyObject* propertyObject)
     for(daqSizeT i = 0; i < count; i++)
     {
         daqList_getItemAt(properties, i, &temp);
-        if (DAQ_SUPPORTS_INTERFACE(temp, DAQ_PROPERTY_INTF_ID))
+        prop = daqBorrowInterfacePtr(temp, DAQ_PROPERTY_INTF_ID);
+        if (prop != NULL)
         {
             daqQueryInterface(temp, DAQ_PROPERTY_INTF_ID, &prop);
 
             daqProperty_getValueType(prop, &propCoreType);
             daqProperty_getName(prop, &name);
-            printDaqFormattedString("\nProperty Name: %s\n", name);
+            printDaqFormattedString("\n%s\n", name);
             daqReleaseRef(name);
             determinePropertyTypeAndPrint(prop);
             daqReleaseRef(prop);
@@ -80,7 +108,7 @@ void displayPropertyTypes(daqPropertyObject* propertyObject)
     daqReleaseRef(properties);
 }
 
-void displayCommonMetadata(daqProperty* property)
+void printMetadata(daqProperty* property)
 {
     daqBool visible = False;
     daqBool readOnly = False;
@@ -276,12 +304,211 @@ void boolProp(daqProperty* property)
 
 void sparseSelectionProp(daqProperty* property)
 {
+    // Value
+    daqBaseObject* tempObj = NULL;
+    daqProperty_getValue(property, &tempObj);
+    daqInteger* value = NULL;
+    daqQueryInterface(tempObj, DAQ_INTEGER_INTF_ID, &value);
+    daqReleaseRef(tempObj);
 
+    // Selection values
+    daqDict* selectionValues = NULL;
+    daqProperty_getSelectionValues(property, &tempObj);
+    daqQueryInterface(tempObj, DAQ_DICT_INTF_ID, &selectionValues);
+    daqReleaseRef(tempObj);
+
+    daqCoreType tempEnum = daqCtUndefined;
+    daqProperty_getItemType(property, &tempEnum);
+
+    printSelectedValue(selectionValues, tempEnum, value);
+    daqReleaseRef(value);
+
+    // Default Value
+    daqProperty_getDefaultValue(property, &tempObj);
+
+    if (tempObj != NULL)
+        return;
+    
+    daqQueryInterface(tempObj, DAQ_INTEGER_INTF_ID, &value);
+    daqReleaseRef(tempObj);
+
+    printSelectedValue(selectionValues, tempEnum, value);
+    daqReleaseRef(value);
 }
 
 void selectionProp(daqProperty* property)
 {
+    // Value
+    daqBaseObject* tempObj = NULL;
+    daqProperty_getValue(property, &tempObj);
+    daqInteger* value = NULL;
+    daqQueryInterface(tempObj, DAQ_INTEGER_INTF_ID, &value);
+    daqReleaseRef(tempObj);
 
+    // Selection values
+    daqList* selectionValues = NULL;
+    daqProperty_getSelectionValues(property, &tempObj);
+    daqQueryInterface(tempObj, DAQ_LIST_INTF_ID, &selectionValues);
+    daqReleaseRef(tempObj);
+
+    daqCoreType tempEnum = daqCtUndefined;
+    daqProperty_getItemType(property, &tempEnum);
+
+    printSelectedValue(selectionValues, tempEnum, value);
+    daqReleaseRef(value);
+
+    // Default Value
+    daqProperty_getDefaultValue(property, &tempObj);
+
+    if (tempObj != NULL)
+        return;
+
+    daqQueryInterface(tempObj, DAQ_INTEGER_INTF_ID, &value);
+    daqReleaseRef(tempObj);
+
+    printSelectedValue(selectionValues, tempEnum, value);
+    daqReleaseRef(value);
+}
+
+void ratioProp(daqProperty* property)
+{
+    // Value
+    daqBaseObject* tempObj = NULL;
+    daqProperty_getValue(property, &tempObj);
+    daqRatio* temp = NULL;
+    daqQueryInterface(tempObj, DAQ_RATIO_INTF_ID, &temp);
+    daqReleaseRef(tempObj);
+
+    daqInt denominator = 0;
+    daqInt numerator = 0;
+    daqRatio_getDenominator(temp, &denominator);
+    daqRatio_getNumerator(temp, &numerator);
+    daqReleaseRef(temp);
+
+    // Print both
+    printf("- Value:\n");
+    printf(" -- denominator: %lld\n", denominator);
+    printf(" -- numerator: %lld\n", numerator);
+
+    // Default value
+    daqProperty_getDefaultValue(property, &tempObj);
+    
+    if (tempObj == NULL)
+        return;
+
+    daqQueryInterface(tempObj, DAQ_RATIO_INTF_ID, &temp);
+    daqReleaseRef(tempObj);
+    denominator = 0;
+    numerator = 0;
+    daqRatio_getDenominator(temp, &denominator);
+    daqRatio_getNumerator(temp, &numerator);
+    daqReleaseRef(temp);
+
+    // Print both
+    printf("- Default Value:\n");
+    printf(" -- denominator: %lld\n", denominator);
+    printf(" -- numerator: %lld\n", numerator);
+}
+
+void functionProp(daqProperty* property)
+{
+    printf("\nMethod\n");
+}
+
+void objectProp(daqProperty* property)
+{
+    daqBaseObject* tempObj = NULL;
+    daqProperty_getValue(property, &tempObj);
+    daqPropertyObject* temp = NULL;
+    daqQueryInterface(tempObj, DAQ_PROPERTY_OBJECT_INTF_ID, &temp);
+    daqReleaseRef(tempObj);
+    printf("-- Object property Value --\n");
+    printPropertyObject(temp);
+    daqReleaseRef(temp);
+
+    printf("\n");
+    daqProperty_getDefaultValue(property, &tempObj);
+
+    if (tempObj == NULL)
+    {
+        printf("---\n");
+        return;
+    }
+
+    daqQueryInterface(tempObj, DAQ_PROPERTY_OBJECT_INTF_ID, &temp);
+    daqReleaseRef(tempObj);
+    printf("-- Object property Default Value --\n");
+    printPropertyObject(temp);
+    daqReleaseRef(temp);
+    printf("---\n");
+}
+
+void prop(daqProperty* property)
+{
+    // Check for others and display what they are as in functionProp
+    daqCoreType tempCt = daqCtUndefined;
+    daqProperty_getValueType(property, &tempCt);
+    switch(tempCt)
+    {
+        case daqCtStruct:
+        {
+            printf("\nOpenDAQ Struct\n");
+        }
+        case daqCtEnumeration:
+        {
+            printf("\nOpenDAQ Enumeration\n");
+        }
+        case daqCtList:
+        {
+            printf("\nOpenDAQ List\n");
+        }
+        case daqCtDict:
+        {
+            printf("\nOpenDAQ Dictionary\n");
+        }
+        default:
+        {
+            daqProperty* tempProp = NULL;
+            daqProperty_getReferencedProperty(property, &tempProp);
+            if (tempProp != NULL)
+            {
+                daqString* name = NULL;
+                daqProperty_getName(prop, &name);
+                printDaqFormattedString("\nReferenced property: %s\n", name);
+                daqReleaseRef(name);
+                determinePropertyTypeAndPrint(tempProp);
+                daqReleaseRef(tempProp);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+void printSelectedValue(daqBaseObject* container, daqCoreType itemType, daqInteger* keyValue)
+{
+    daqInt key = 0;
+    daqInteger_getValue(keyValue, key);
+
+    daqBaseObject* temp = NULL;
+
+    if(DAQ_SUPPORTS_INTERFACE(container, DAQ_LIST_INTF_ID))
+    {
+        daqList* tempContainer = NULL;
+        daqQueryInterface(container, DAQ_LIST_INTF_ID, &tempContainer);
+        daqList_getItemAt(tempContainer, key, &temp);
+        daqReleaseRef(tempContainer);
+    }
+    else
+    {
+        daqDict* tempContainer = NULL;
+        daqQueryInterface(container, DAQ_DICT_INTF_ID, &tempContainer);
+        daqDict_get(tempContainer, keyValue, &temp);
+        daqReleaseRef(tempContainer);
+    }
+    
 }
 
 int main()
@@ -293,7 +520,7 @@ int main()
     daqDevice* simulator = NULL;
     addSimulator(&simulator, &instance);
 
-    displayPropertyTypes((daqPropertyObject*) simulator);
+    printPropertyObject((daqPropertyObject*) simulator);
 
     daqReleaseRef(simulator);
     daqReleaseRef(instance);
