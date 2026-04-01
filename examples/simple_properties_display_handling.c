b8431897e3c465ec263769b92c4b50eafba9f180
Aljaž Zakošek <aljaz.zakosek@gmail.com>
2026-04-01 09:03:41 +0200

    Refactor previous state

--- examples/simple_properties_display_handling.c
+++ examples/simple_properties_display_handling.c
@@ -24,12 +24,15 @@ void printPropertyTree(daqPropertyObject* propertyObject)
         daqCoreType valueType = daqCtUndefined;
         daqProperty_getValueType(property, &valueType);
 
+        daqPropertyType propType;
+        daqProperty_getPropertyType(property, &propType);
+
         daqBaseObject* propertyValue;
         daqProperty_getValue(property, &propertyValue);
         
         printPropertyMetadata(property);
 
-        if (valueType == daqCtObject)
+        if (propType == daqPropertyTypeObject)
         {
             printf("---\n");
             daqPropertyObject* childPropertyObject = NULL;
